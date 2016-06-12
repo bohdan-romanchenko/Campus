1e96a59604e32b37eaa1e6aaae20b43e46aeff32
nadman <nadman3@gmail.com>
2016-06-12 11:38:55 +0300

    initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Bohdan Romanchenko (nadman)
+ * Date : 6/12/16.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Q");
+    }
+}
